6ef3ca32e0a10df4d666405420bb2a32164f2cff
Alison Myers <alison.myers@sauder.ubc.ca>
2022-06-08 13:48:29 -0700

    restructure src

--- rubric_assessment_app.py
+++ rubric_assessment_app.py
@@ -1,3 +1,9 @@
+import os
+import sys
+module_path = os.path.abspath(os.path.join('src/'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
 import random
 import pandas as pd
 import json
