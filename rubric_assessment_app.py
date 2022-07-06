c33e6fed8364efd0396f7bcf78bc8d6e81fa264f
IvyaThalenmourn <44929143+IvyaThalenmourn@users.noreply.github.com>
2022-07-06 09:49:46 -0700

    Issue #5: code-cleanup
    Cleaned up the code to conform with PEP8 standard, added docstrings to functions and modules, added comments, removed unused functions, modified names to more meaningful names, modified some html.ids.

--- rubric_assessment_app.py
+++ rubric_assessment_app.py
@@ -1,17 +1,17 @@
+"""Module that handles the display and interaction with the script.
+
+This module raises the exception PreventUpdate when it counters a lack
+of data for a display of a rubric assessment or a .csv download.
+"""
+
 import os
 import sys
 module_path = os.path.abspath(os.path.join("src/"))
 if module_path not in sys.path:
     sys.path.append(module_path)
-
-import random
 import pandas as pd
-import json
-import os
-from helpers import create_instance, _return_single_dict_match, get_rubric_assessment, get_output_data
-from initial_requests import get_initial_info
 
-#canvasapi 
+# canvasapi 
 from canvasapi import Canvas
 
 # DASH
@@ -20,23 +20,24 @@ from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
-# Env details
+# Local imports
+from helpers import create_instance, return_single_dict_match, get_output_data
+from initial_requests import get_initial_info
+
+# Environment details
 from dotenv import load_dotenv
 load_dotenv() 
 
 URL = os.getenv("API_INSTANCE")
 KEY = os.getenv("API_TOKEN")
-COURSE_ID = os.getenv("COURSE_ID")
 GRAPH_URL = f"{URL}/api/graphql"
 
 print(GRAPH_URL)
 
-
 canvas = create_instance(URL, KEY)
 
-def drop_down_div(list_of_dicts, dropdown_id, div_id):
-    """
-    """
+
+def _drop_down_div(list_of_dicts, dropdown_id, div_id):
     first_value = list_of_dicts[0].get("value")
     
     html_div = html.Div([
@@ -47,11 +48,10 @@ def drop_down_div(list_of_dicts, dropdown_id, div_id):
     return(html_div)
 
 def app():
-
-    # Get course id
-    # once confirmed move to next step
-
-    # 
+    ''' Format page and allow user to interact.  Confirm course id first.
+    Select and show assignments and associated rubrics.  Allow download 
+    of rubric .csv file.
+    '''
     app = JupyterDash(__name__)
     app.config.suppress_callback_exceptions = True
 
@@ -63,44 +63,56 @@ def app():
                     html.H2("Input Course ID"),
                     html.Div(
                         children = [
-                            dcc.Input(id="input-course-id", type="number", style={"display": "inline-block"}),
-                            html.Button("Submit", id="submit-course-id", n_clicks=0, style={"display": "inline-block"})
-                            ])]),
-            html.Div(children="Enter your course id", id="course-details-display"),
+                            dcc.Input(id="input-course-id", type="number",
+                            style={"display": "inline-block"}),
+                            html.Button("Submit", id="submit-course-id",
+                            n_clicks=0, style={"display": "inline-block"})
+                        ]
+                    )
+                ]
+            ),
+            html.Div(children="Enter your course id",
+                     id="course-details-display"),
             html.Br(),
             html.Div(children=[], id="confirmed-course"),
         ], 
-        id="initial-input-course")
+        id="initial-input-course"
+    )
         
     @app.callback(
         Output("course-details-display", "children"),
         Input("submit-course-id", "n_clicks"),
         State("input-course-id", "value")
-        )
+    )
 
-    def update_output(n_clicks, value):
-
+    def access_canvas(n_clicks, value):
+        # Confirm access token validity.
+        # Query user for details to access Canvas course information.
         if canvas==None:
             return(
-                html.P(f"Error creating session. Confirm you have an active token and a green confirmation at the top noting 'Token Valid: ... '", style={"color": "red"})
+                html.P(f"""Error creating session. Confirm you have an active 
+                token and a green confirmation at the top noting 'Token Valid: 
+                ... '""", style={"color": "red"})
             )
 
-
         if n_clicks > 0:
-
             try:
                 course = canvas.get_course(value)
                 return(
                     html.Div(children = [
-                        html.P(f"You have selected: {course.name}", style={"color": "green"}),
-                        html.Button(f"Confirm {course.name}", id="confirm-course", n_clicks=0),
+                        html.P(f"You have selected: {course.name}",
+                               style={"color": "green"}),
+                        html.Button(f"Confirm {course.name}",
+                                    id="confirm-course", n_clicks=0),
                         dcc.Store(id="course-data")
-                    ])
+                        ]
                     )
+                )
             except Exception as err:
                 return(
-                    html.P(f"Error with course id {value}:\n{err}\nPlease submit another course id.", style={"color": "red"})
-                    )
+                    html.P(f"""Error with course id {value}:\n{err}\nPlease 
+                    submit another course id.""", style={"color": "red"})
+                )
             
         else:
             return(f"Please enter a course ID and press submit :D")
@@ -112,18 +124,25 @@ def app():
         State("input-course-id", "value")
     )
 
-    def the_course_has_been_confirmed(n_clicks, value):
+    def course_is_confirmed(n_clicks, value):
+        # Display list of assignments in confirmed course
         if n_clicks >= 1:
             data = get_initial_info(GRAPH_URL, int(value), KEY)
             assignments = data["data"]["course"]["assignmentsConnection"]["nodes"]
             #TODO only return assignments with rubrics in list
-            assignments_list = [{"label": i.get("name"), "value": i.get("_id")} for i in assignments]
+            assignments_list = [{"label": i.get("name"), "value": i.get("_id")}
+                                for i in assignments
+            ]
 
-            new_div = html.Div(children=[
-                drop_down_div(assignments_list, "assignments-dropdown", "assignments-dropdown-container"),
-                html.Div(children=[], id="selected-assignment"), 
-                dcc.Store(id="reviews-data")],
-                id="assignments-selector-container")
+            new_div = html.Div(
+                children=[
+                    _drop_down_div(assignments_list, "assignments-dropdown",
+                                  "assignments-dropdown-container"),
+                    html.Div(children=[], id="selected-assignment"), 
+                    dcc.Store(id="reviews-data")
+                ],
+                id="assignments-selection-container"
+            )
             
             return(new_div, data)
 
@@ -131,27 +150,23 @@ def app():
             raise PreventUpdate
 
     @app.callback(
-        [Output("selected-assignment", "children"),
-        Output("reviews-data", "data")],
-        [Input("assignments-dropdown", "value"),
-        Input("course-data", "data")]
+        [Output("selected-assignment", "children"), Output("reviews-data", "data")],
+        [Input("assignments-dropdown", "value"), Input("course-data", "data")]
     )
 
     def show_selected_assignment(assignment_value, data):
-
+        # Display dataframe for user-selected assignment
         if data is None:
             raise PreventUpdate
 
         else:
-
             assignments_info = data["data"]["course"]["assignmentsConnection"]["nodes"]
-            assignment = _return_single_dict_match(assignments_info, "_id", str(assignment_value))
+            assignment = return_single_dict_match(assignments_info, "_id", str(assignment_value))
 
             assignment_name = assignment.get("name")
             rubric = assignment.get("rubric")
 
             if rubric is None:
-                rubric_title = "No Rubric"
                 return(html.P("No rubric found for this assignment."), None)
 
             else:    
@@ -163,31 +178,37 @@ def app():
                     #TODO check for incomplete rubrics
                     submissions = assignment.get("submissionsConnection").get("nodes")
 
-                    #reviews_list = [get_rubric_assessment(i) for i in submissions]
-                    #print(get_output_data(submissions))
-                    reviews_list = get_output_data(submissions)
-                    
+                    reviews_list = get_output_data(submissions)                    
 
                     df = pd.DataFrame(reviews_list)
-                    #df = df.drop(["points", "descriptions", "comments"], axis=1)
 
-                    new_html = html.Div([html.H3(f"{assignment_name} ({assignment_value})"),
-                    html.H4(f"Rubric: {rubric_title}"),
-                    dash_table.DataTable(df.to_dict("records"), [{"name": i, "id": i} for i in df.columns]),
-                    html.Br(),
-                    html.Div(
-                        [
-                            html.Button("Download CSV", id="btn_csv", n_clicks=0),
+                    new_html = html.Div([
+                        html.H3(f"""{assignment_name} ({assignment_value})"""),
+                        html.H4(f"Rubric: {rubric_title}"),
+                        dash_table.DataTable(
+                            df.to_dict("records"),
+                            [{"name": i, "id": i} for i in df.columns]
+                        ),
+                        html.Br(),
+                        html.Div([
+                            html.Button("Download CSV", id="csv_download_button", n_clicks=0),
                             dcc.Download(id="download-dataframe-csv"),
                             html.Div(children=[], id="final-output-container")
-                        ])], id="returning-assignment-details")
+                            ])
+                        ], id="returning-assignment-details"
+                    )
 
                     return(new_html, reviews_list)
 
                 except Exception as err:
-                    return(html.Div([html.H3(f"{assignment_name} ({assignment_value})"), html.H4(f"Rubric: {rubric_title}"), 
-                    html.P(f"This rubric has no assessment data. {err}")]), 
-                    None)
+                    return(
+                        html.Div([
+                            html.H3(f"""{assignment_name} ({assignment_value})"""),
+                            html.H4(f"Rubric: {rubric_title}"), 
+                            html.P(f"This rubric has no assessment data. {err}")
+                            ]),
+                            None
+                    )
 
     @app.callback(
         Output("final-output-container", "children"),
@@ -198,23 +219,20 @@ def app():
     )
 
     def save_csv(reviews_data, button_clicks):
-
+        # Download .csv file of dataframe
         if reviews_data is None:
             raise PreventUpdate
 
         elif button_clicks > 0:
             df = pd.DataFrame(reviews_data)
             csv_name = "my_csv.csv"
-            return(f"Complete! See csv: {csv_name}", dcc.send_data_frame(df.to_csv, csv_name))
+            return(
+                f"Complete! See csv: {csv_name}",
+                dcc.send_data_frame(df.to_csv, csv_name)
+            )
         
         else:
            raise PreventUpdate
 
-
-
     app.run_server(mode="inline")
-
-    
-
-    
 
--- src/initial_requests.py
+++ src/initial_requests.py
@@ -1,112 +1,134 @@
-import requests
-import json
-import os
+"""Module that handles initial course authorization requests.
+
+This module exports the following functions:
+    get_initial_info - sends a query for Canvas assessment details.
+
+An exception is raised when the user is not authorized for the course 
+they are trying to access. The function prints an indicative message 
+instead of proceeding.
+"""
 
 # uses GRAPH QL
 
+import requests
+
 def _get_query_by_course(url, query, course_id, KEY):
-    
-    r = requests.post(url, json={"query": query, "variables": {"id": f"{course_id}"}}, headers={"Authorization": f"Bearer {KEY}"})
+    # Return answer to query for course information if user is authorized.
+    # Print error if user us not authorized to access the course.   
+    r = requests.post(
+        url, json={"query": query, "variables": {"id": f"{course_id}"}},
+        headers={"Authorization": f"Bearer {KEY}"}
+    )
+
     if r.status_code == 200:
         return(r.json())
+    
     else:
         print(f"Error: {r.json()}")
 
-def get_initial_info(url, course_id, KEY):
 
+def get_initial_info(url, course_id, KEY):
+    """Send initial query for Canvas assessment information.
+    
+    parameters:
+        url (str): URL of Canvas instance
+        course_id (int): integer indicating the course number
+        KEY (str): access token for authorization
+    returns:
+        all_json: json encoded response
+
+    exceptions:
+        If the user is not authorized to access that course, print a message.
+    """
     query = """query($id: ID){
-    __typename
-    course(id: $id) {
-        id
-        _id
-        name
-        assignmentsConnection {
-        nodes {
+        __typename
+        course(id: $id) {
+            id
             _id
             name
-            dueAt
-            expectsSubmission
-            pointsPossible
-            state
-            rubric {
-            id
-            pointsPossible
-            title
-            _id
-            criteria {
-                _id
-                description
-                longDescription
-                points
-                ratings {
-                _id
-                longDescription
-                description
-                points
-                }
-            }
-            }
-            submissionsConnection {
-            nodes {
-                _id
-                score
-                state
-                submissionStatus
-                submittedAt
-                attempt
-                grade
-                user {
-                name
-                _id
-                sisId
-                }
-                commentsConnection {
+            assignmentsConnection {
                 nodes {
-                    comment
-                    author {
+                    _id
                     name
-                    _id
-                    }
-                }
-                }
-                rubricAssessmentsConnection {
-                nodes {
-                    _id
-                    assessor {
+                    dueAt
+                    expectsSubmission
+                    pointsPossible
+                    state
+                    rubric {
+                        id
+                        pointsPossible
+                        title
                         _id
-                        name
+                        criteria {
+                            _id
+                            description
+                            longDescription
+                            points
+                            ratings {
+                                _id
+                                longDescription
+                                description
+                                points
+                            }
+                        }
                     }
-                    assessmentRatings {
-                    _id
-                    comments
-                    commentsHtml
-                    description
-                    points
-                    criterion {
-                        _id
-                        description
-                        longDescription
-                        points
+                    submissionsConnection {
+                        nodes {
+                            _id
+                            score
+                            state
+                            submissionStatus
+                            submittedAt
+                            attempt
+                            grade
+                            user {
+                                name
+                                _id
+                                sisId
+                            }
+                            commentsConnection {
+                                nodes {
+                                    comment
+                                    author {
+                                        name
+                                        _id
+                                    }
+                                }
+                            }
+                            rubricAssessmentsConnection {
+                                nodes {
+                                    _id
+                                    assessor {
+                                        _id
+                                        name
+                                    }
+                                    assessmentRatings {
+                                        _id
+                                        comments
+                                        commentsHtml
+                                        description
+                                        points
+                                        criterion {
+                                            _id
+                                            description
+                                            longDescription
+                                            points
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                    }
-                }
                 }
             }
-            }
         }
-        }
-    }
     }
     """
 
     try:
-        
         all_json = _get_query_by_course(url, query, course_id, KEY)
-        #assignments = all_json["data"]["course"]["assignmentsConnection"]["nodes"]
-        #group_sets = all_json["data"]["course"]["groupSetsConnection"]["nodes"]
-        #course = { your_key: all_json["data"]["course"][your_key] for your_key in ["_id", "id", "courseCode", "name"] }
-        #users = all_json["data"]["course"]["usersConnection"]["nodes"]
         return(all_json)
         
     except Exception as err:
         print(f"Error: {err}")
+
